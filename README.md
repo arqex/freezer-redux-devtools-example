9a65dae7f0ae8e6cba4ae5abb0c2c7a12bda9dfa
marquex <javi@arqex.com>
2015-11-03 17:47:36 +0100

    Updates freezer-redux-devtools to make it compatible with react-devtools extension.

--- README.md
+++ README.md
@@ -2,9 +2,11 @@
 
 This is a demo on how to use [freezer](https://github.com/arqex/freezer) and [redux-devtools](https://github.com/gaearon/redux-devtools) together, thanks to [freezer-redux-devtools](https://github.com/arqex/freezer-redux-devtools).
 
+It supports the superb [redux-devtools chrome extension](https://github.com/zalmoxisus/redux-devtools-extension). So if you have it installed you can update the state of the app using the extension.
+
 ### [Check the live demo](http://freezer-redux-devtools.divshot.io/)
 
-How to use:
+Want to play with the code?
 -----------
 ```
 $ git clone https://github.com/arqex/freezer-redux-devtools-example.git
--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,7 @@
-
 import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react';
 import React, { Component } from 'react';
+
+import { supportChromeExtension } from 'freezer-redux-devtools/freezer-redux-middleware';
 import { getStore } from 'freezer-redux-devtools';
 
 import CounterApp from './CounterApp';
@@ -10,10 +11,22 @@ import State from './State';
 // Bind the reactions
 import './reactions/CounterReactions';
 
-// use freezer-redux-devtools to create a redux store for
-// the dev tools
-const store = getStore( State );
 
+var tools = '';
+if( window.devToolsExtension ){
+	// Use the extension if available
+	supportChromeExtension( State );
+}
+else {
+	// use freezer-redux-devtools to create a redux store for
+	// the dev tools
+	const store = getStore( State );
+	tools = (
+		<DebugPanel top right bottom>
+			<DevTools store={store} monitor={LogMonitor} />
+		</DebugPanel>
+	);
+}
 
 /**
  * Root component including devtools
@@ -24,9 +37,7 @@ export default class App extends Component {
     return (
       <div>
       	<CounterApp state={ State.get() } />
-			<DebugPanel top right bottom>
-				<DevTools store={store}	monitor={LogMonitor} />
-			</DebugPanel>
+	     	{ tools }
       </div>
     );
   }
